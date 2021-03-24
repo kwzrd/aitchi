169eeb7143f3ce379d3e57205b2df6c5669b90c8
kwzrd <kwzrd@pythondiscord.com>
2021-03-24 22:10:23 +0100

    TikTok: implement fetch helper

--- aitchi/extensions/tiktok.py
+++ aitchi/extensions/tiktok.py
@@ -1,8 +1,10 @@
 import logging
+import typing as t
 
 from discord.ext import commands
 
 from aitchi.aitchi import Aitchi
+from aitchi.config import Env
 from aitchi.persistence import Store
 
 log = logging.getLogger(__name__)
@@ -22,6 +24,25 @@ class TikTok(commands.Cog):
         """Initialise store."""
         self.bot = bot
         self.store = Store(namespace="tiktok")
+
+    async def fetch_videos(self) -> t.Dict[str, t.Any]:
+        """
+        Poll TikTok API for the last 10 videos from the configured user.
+
+        Raises an exception on non-200 responses. Otherwise, the JSON response is returned as a Python object.
+        """
+        log.info("Polling API for videos!")
+
+        params = {
+            "count": 10,  # How many videos to get
+            "cursor": 0,  # Starting from 0th video, i.e. last 10
+            "aid": Env.tiktok_aid,  # Application ID
+            "secUid": Config.tiktok_user,  # Target user
+        }
+        async with self.bot.http_session.get("https://m.tiktok.com/api/post/item_list", params=params) as resp:
+            if resp.status != 200:
+                raise Exception(f"Failed to get video list due to status: {resp.status}")
+            return await resp.json()
 
 
 def setup(bot: Aitchi) -> None:
